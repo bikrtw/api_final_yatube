420e0515159d3ecfe5a7adaad9e701f4233ff7c1
Игорь Бутаков <bikrtw@gmail.com>
2022-03-09 15:54:54 +0300

    pep8

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -21,23 +21,23 @@ urlpatterns = [
 ]
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Posts API",
-      default_version='v1',
-      description="Документация для приложения posts проекта Yatube",
-      # terms_of_service="URL страницы с пользовательским соглашением",
-      contact=openapi.Contact(email="test@test.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Posts API",
+        default_version='v1',
+        description="Документация для приложения posts проекта Yatube",
+        # terms_of_service="URL страницы с пользовательским соглашением",
+        contact=openapi.Contact(email="test@test.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns += [
-   url(r'^v1/swagger(?P<format>\.json|\.yaml)$',
-       schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   url(r'^v1/swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-       name='schema-swagger-ui'),
-   url(r'^v1/redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-       name='schema-redoc'),
+    url(r'^v1/swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^v1/swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    url(r'^v1/redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
 ]
